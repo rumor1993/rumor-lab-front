b37578ce1e6df3bf1468e182ac5b7293eb96ca0d
rumor1993 <rumor1993@naver.com>
2023-04-06 23:13:51 +0900

    위치 조정

--- src/routes/Body.svelte
+++ src/routes/Body.svelte
@@ -1,6 +1,6 @@
 <body>
 <div class="grid place-items-center mt-10">
-    <div class="text-4xl font-bold text-center"> AI로 나의 얼굴을 <br> 만화스타일로 바뀐다면?</div>
+    <div class="text-4xl mx-5 font-bold text-center"> AI로 나의 얼굴을 <br> 만화스타일로 바뀐다면?</div>
     <div class="text-1xl text-center mt-5 ml-6 mr-6 text-gray-500"> 자신이나 친구, 가족들의 사진을 올려보세요! <br> AI가 실제 사진을 만화 스타일의 모습으로 그려준 이미지를
         공유할 수 있습니다.
     </div>
@@ -17,7 +17,7 @@
     </div>
 
     <div class="grid place-items-center mt-10">
-        <div class="text-4xl font-bold text-center"> 이모지를 쉽고 빠르게 <br> 만들어서 사용하세요!</div>
+        <div class="text-4xl mx-5 font-bold text-center"> 이모지를 쉽고 <br> 빠르게 만들어서 사용하세요!</div>
         <div class="text-1xl text-center mt-5 ml-6 mr-6 text-gray-500"> 슬랙, 텔레그램, 카카오톡등 <br> 직접 만든 이모지를 활용하여 채팅할 수 있습니다.</div>
         <div class="svelte-card w-96 h-96 mt-11 bg-white rounded-lg shadow-lg bg-[url('/src/lib/images/emoji.png')] bg-center bg-cover">
         </div>
